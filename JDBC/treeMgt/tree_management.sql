401dbd6b4f2b9ed9e425aee61103b784f949a8cf
skt231 <112694610+skt231@users.noreply.github.com>
2022-12-17 02:16:57 +0900

    tree_management
    sql

---
+++ JDBC/treeMgt/tree_management.sql
@@ -0,0 +1,17 @@
+drop table tree;
+CREATE TABLE TREE (
+name NVARCHAR2(20), 
+price NUMBER(10,0), 
+growtime DATE, 
+exp NUMBER(3,0), 
+nowplant NUMBER(3,0), 
+maxplant NUMBER(3,0)
+);
+commit;
+
+
+
+
+
+
+	

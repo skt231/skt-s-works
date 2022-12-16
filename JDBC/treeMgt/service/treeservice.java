02a623942fc48fc281f905de64788c8d983c31f1
skt231 <112694610+skt231@users.noreply.github.com>
2022-12-17 02:13:31 +0900

    treeService
    Service

---
+++ JDBC/treeMgt/service/treeservice.java
@@ -0,0 +1,32 @@
+package treeMgt.service;
+
+import java.util.ArrayList;
+
+import treeMgt.dao.Treedao;
+import treeMgt.dto.Treedto;
+
+public class treeservice {
+	private Treedao dao=new Treedao();
+	
+	public void init() {
+		dao.init();
+	}
+	public void insert(Treedto dto) {
+		dao.insert(dto);
+	}
+	public void update(String name, int nowplant) {
+		dao.update(name,nowplant);
+	}
+	public void delete(Integer name) {
+		dao.delete(name);
+	}
+	public ArrayList<Treedto> select(){
+		return dao.select();
+	}
+	public ArrayList<Treedto> select(int nowplant){
+		return dao.select(nowplant);
+		
+	}
+
+
+}
